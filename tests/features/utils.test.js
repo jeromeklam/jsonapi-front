db8a5dabbfb1e59e62c79dff5734d7defa1f5b35
Utilisateur <Utilisateur@PC3259.mshome.net>
2021-04-10 19:26:56 +0200

    deepClone

--- tests/features/utils.test.js
+++ tests/features/utils.test.js
@@ -1,4 +1,4 @@
-import { lambert93toWGPS } from '../../src/features/tools/utils.js';
+import { deepClone,lambert93toWGPS } from '../../src/features/tools/utils.js';
 
 /**
  * lambert93toWGPS
@@ -10,4 +10,24 @@ test('lambert93toWGPS : Verify ""', () => {
 test('lambert93toWGPS : Verify ""', () => {
   const result = lambert93toWGPS(961055.157, 6468811.914);
   expect(result).toEqual([6.329885894853984, 45.269110416825335]);
+});
+
+/**
+ * deepClone
+ */
+test('deepClone : Verify null', () => {
+  const result = deepClone(null);
+  expect(result).toEqual(null);
+});
+test('deepClone : Verify string', () => {
+  const result = deepClone('45');
+  expect(result).toEqual('45');
+});
+test('deepClone : Verify simple object', () => {
+  const result = deepClone({id: '45'});
+  expect(result).toEqual({id: '45'});
+});
+test('deepClone : Verify simple array', () => {
+  const result = deepClone(['45', '55']);
+  expect(result).toEqual(['45', '55']);
 });
